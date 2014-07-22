1757083a06ec088ccaa1143e4d5bf1850989accd
consti <consti@consti.de>
2014-07-22 19:54:34 +0700

    validate school year range and add inputs for dates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
-
   TITLES = ["BA", "BSc", "DI", "DI Dr.", "DI FH", "DI Mag.", "Dkfm.", "Dr.",
-            "Ing.", "MA", "Mag.", "Mag. Dr.", "Mag. FH", "MAS", "MBA", "MD", "MMag.",
-            "MMag. Dr.", "PhD.", "Prof.", "Prof. Dr.", "Univ.-Prof.", "Univ.-Prof. Dr."].sort
-
+            "Ing.", "MA", "Mag.", "Mag. Dr.", "Mag. FH", "MAS", "MBA", "MD",
+            "MMag.", "MMag. Dr.", "PhD.", "Prof.", "Prof. Dr.", "Univ.-Prof.",
+            "Univ.-Prof. Dr."].sort
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -11,6 +10,16 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   validates :first_name, :last_name, presence: true
   validates :school_year_begin, :school_year_end, presence: true
-  validates :title, inclusion: { in: TITLES,
-    message: "%{value} is not a valid title" }, allow_blank: true
+  validate :school_year_end_cannot_be_before_school_year_begin
+  validates :title,
+            inclusion: { in: TITLES, message: "%{value} is not a valid title" },
+            allow_blank: true
+
+  protected
+
+  def school_year_end_cannot_be_before_school_year_begin
+    return if self.school_year_end > self.school_year_begin
+    self.errors.add(:school_year_end, 'cannot be before school year begin')
+  end
+
 end

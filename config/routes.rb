1aa9033463afb6febab1c0a2bdccd1c4911225ca
consti <consti@consti.de>
2014-07-26 17:09:15 +0700

    locations index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'welcome#index'
 
   devise_for :users
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :graduating_classes, only: [:index]
   get 'class_of/:year' => 'graduating_classes#show', as: :graduating_class
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
